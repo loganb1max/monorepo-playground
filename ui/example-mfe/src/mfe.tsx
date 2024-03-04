4a7c5b62afa47f9787211cdfbaa3168ebb14c0b2
Logan Barrett <loganb1max@users.noreply.github.com>
2024-03-04 02:17:26 -0600

    add new message to mfe (#1)
    Co-authored-by: GitHub Actions <gha@users.noreply.github.com>

--- ui/example-mfe/src/mfe.tsx
+++ ui/example-mfe/src/mfe.tsx
@@ -1,5 +1,9 @@
+import { FormattedMessage } from "react-intl";
+
 export const MFE = () => (
   <div>
-    <h1>MFE</h1>
+    <h1>
+      <FormattedMessage defaultMessage={"Hello from MFE!"} />
+    </h1>
   </div>
 );
